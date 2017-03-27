31bc5ff3a12046bc37d3c8267379b2f7db913625
Hizuro <hizuro@gmx.net>
2017-03-27 20:16:14 +0200

    updated localization (deDE)

--- TooltipRealmInfo.lua
+++ TooltipRealmInfo.lua
@@ -66,21 +66,26 @@ if LOCALE_deDE then
 	L["AddOn loaded..."] = "Addon geladen..."
 	L["'AddOn loaded...' message:"] = "'Addon geladen...' Nachricht:"
 	L["battlegroup"] = "Schlachtgruppe"
+	L["Behind language in line 'Realm language'"] = "Hinter der Sprache in der Zeile Realmsprache"
+	L["Behind the character name"] = "Hinter dem Charakternamen"
 	L["Chat command list for /ttri or /tooltiprealminfo"] = "Chatbefehlsliste für /ttri oder /tooltiprealminfo"
 	L["Connected realms"] = "Verbundene Realms"
 	L["Country flag"] = "Landesflagge"
-	L["Currently doesn't work with TipTac"] = "Funktioniert zur Zeit nicht mit TipTac"
+	L["Currently doesn't work with TipTac"] = "Funktioniert zurzeit nicht mit TipTac"
 	L["Display the country flag without text on the left side in tooltip"] = "Zeige Landesflagge ohne Text auf der rechten Seite im Tooltip"
 	L["For options use /ttri or /tooltiprealminfo"] = "Gib /ttri oder /tooltiprealminfo ein, um zu den Optionen zu gelangen"
 	L["Hide %s in tooltip"] = "%s im Tooltip verstecken"
 	L["In own tooltip line on the left site"] = "In eigener Tooltipzeile auf der rechten Seite"
 	L["Open option panel"] = "Öffne Optionpanel"
+	L["Prepend country flag on character name"] = "Die Landesflagge dem Charakternamen voranstellen"
 	L["Realm battlegroup"] = "Realm Schlachtgruppe"
 	L["Realm language"] = "Realmsprache"
 	L["Realm timezone"] = "Realmzeitzone"
 	L["Realm type"] = "Realmtyp"
 	L["Show %s in tooltip"] = "%s im Tooltip zeigen"
 	L["timezone"] = "Zeitzone"
+	L["Toggle 'AddOn loaded...' message"] = "Die Nachricht \"Addon geladen\" ein-/ausschalten"
+	L["Tooltip"] = "Tooltip"
 	L["Tooltip line '%s' is now hidden"] = "Tooltipzeile '%s' wird jetzt versteckt"
 	L["Tooltip line '%s' is now shown"] = "Tooltipzeile '%s' wird jetzt gezeigt"
 elseif LOCALE_esES or LOCALE_esMX then
@@ -251,7 +256,7 @@ end
 GameTooltip:HookScript("OnTooltipSetUnit",function(self,...)
 	local name, unit, guid, realm = self:GetUnit(); 
 	if not unit then
-		mf = GetMouseFocus();
+		local mf = GetMouseFocus();
 		if mf then unit = mf.unit end
 	end
 	if unit and UnitIsPlayer(unit) then
