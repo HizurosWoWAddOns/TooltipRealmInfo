241e94ab0924f24f17ced5e66b81cabbec780d19
Hizuro <hizuro@gmx.net>
2022-06-13 04:26:16 +0200

    add shared tools and credit page

--- TooltipRealmInfo.lua
+++ TooltipRealmInfo.lua
@@ -3,6 +3,8 @@ TooltipRealmInfoDB = {};
 local addon, ns = ...;
 local L = ns.L;
 local C = WrapTextInColorCode;
+ns.addon_short = "TTRI"
+ns.debugMode = "@project-version@" == "@".."project-version".."@"
 
 -- very nice addon from Phanx :) Thanks...
 local LRI = LibStub("LibRealmInfo");
@@ -130,34 +132,6 @@ local replaceRealmNames	 = { -- <api> = <LibRealmInfo compatible>
 	["ThrokFeroth"] = "Throk'Feroth", ["TwilightsHammer"] = "Twilight's Hammer", ["TwistingNether"] = "Twisting Nether", ["UnGoro"] = "Un'Goro",
 	["Veklor"] = "Vek'lor", ["Veknilash"] = "Vek'nilash", ["Voljin"] = "Vol'jin", ["ZirkeldesCenarius"] = "Zirkel des Cenarius", ["Zuljin"] = "Zul'jin"
 };
-
-do
-	local addon_short = "TTRI";
-	local colors = {"82c5ff","00ff00","ff6060","44ffff","ffff00","ff8800","ff44ff","ffffff"};
-	local function colorize(...)
-		local t,c,a1 = {tostringall(...)},1,...;
-		if type(a1)=="boolean" then tremove(t,1); end
-		if a1~=false then
-			tinsert(t,1,"|cff82c5ff"..((a1==true and addon_short) or (a1=="||" and "||") or addon).."|r"..(a1~="||" and HEADER_COLON or ""));
-			c=2;
-		end
-		for i=c, #t do
-			if not t[i]:find("\124c") then
-				t[i],c = "|cff"..colors[c]..t[i].."|r", c<#colors and c+1 or 1;
-			end
-		end
-		return unpack(t);
-	end
-	function ns.print(...)
-		print(colorize(...));
-	end
-	function ns.debug(...)
-		ConsolePrint(date("|cff999999%X|r"),colorize(...));
-	end
-	function ns.debugPrint(...)
-		print(colorize("<debug>",...));
-	end
-end
 
 local function GetRealmInfo(object)
 	if not (type(object)=="string" and object:trim():len()>0) then
@@ -683,13 +657,19 @@ local options = {
 				},
 
 			}
-		}
+		},
+		credits = {
+			type = "group", order = 200,
+			name = L["Credits"],
+			args = {}
+		},
 	}
 };
 
 local function RegisterOptionPanel()
 	LibStub("AceConfig-3.0"):RegisterOptionsTable(addon, options);
 	LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addon);
+	ns.AddCredits(options.args.credits.args);
 end
 
 local function RegisterSlashCommand()
